bf26061ff44b76583ddccbb3e0f0d1d54f02c7ee
Protetor074 <101059740+Protetor074@users.noreply.github.com>
2023-10-17 18:05:55 +0200

    Lab 1 - Praca zakoczona z zajec

--- Urządzenia Peryferyjne Lab - 1 - Skaner/Urządzenia Peryferyjne Lab - 1 - Skaner/Form1.Designer.cs
+++ Urządzenia Peryferyjne Lab - 1 - Skaner/Urządzenia Peryferyjne Lab - 1 - Skaner/Form1.Designer.cs
@@ -57,9 +57,10 @@
             // 
             // buttonScan
             // 
-            this.buttonScan.Location = new System.Drawing.Point(9, 394);
+            this.buttonScan.Location = new System.Drawing.Point(19, 758);
+            this.buttonScan.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonScan.Name = "buttonScan";
-            this.buttonScan.Size = new System.Drawing.Size(75, 23);
+            this.buttonScan.Size = new System.Drawing.Size(149, 44);
             this.buttonScan.TabIndex = 0;
             this.buttonScan.Text = "Skanuj";
             this.buttonScan.UseVisualStyleBackColor = true;
@@ -67,9 +68,10 @@
             // 
             // buttonRL_180
             // 
-            this.buttonRL_180.Location = new System.Drawing.Point(66, 334);
+            this.buttonRL_180.Location = new System.Drawing.Point(132, 642);
+            this.buttonRL_180.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRL_180.Name = "buttonRL_180";
-            this.buttonRL_180.Size = new System.Drawing.Size(39, 29);
+            this.buttonRL_180.Size = new System.Drawing.Size(77, 56);
             this.buttonRL_180.TabIndex = 5;
             this.buttonRL_180.Text = "180°";
             this.buttonRL_180.UseVisualStyleBackColor = true;
@@ -78,26 +80,29 @@
             // labelRotationLeft
             // 
             this.labelRotationLeft.AutoSize = true;
-            this.labelRotationLeft.Location = new System.Drawing.Point(51, 318);
+            this.labelRotationLeft.Location = new System.Drawing.Point(101, 611);
+            this.labelRotationLeft.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelRotationLeft.Name = "labelRotationLeft";
-            this.labelRotationLeft.Size = new System.Drawing.Size(69, 13);
+            this.labelRotationLeft.Size = new System.Drawing.Size(136, 25);
             this.labelRotationLeft.TabIndex = 7;
             this.labelRotationLeft.Text = "Obrót w lewo";
             // 
             // labelRotationRight
             // 
             this.labelRotationRight.AutoSize = true;
-            this.labelRotationRight.Location = new System.Drawing.Point(50, 262);
+            this.labelRotationRight.Location = new System.Drawing.Point(100, 504);
+            this.labelRotationRight.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelRotationRight.Name = "labelRotationRight";
-            this.labelRotationRight.Size = new System.Drawing.Size(76, 13);
+            this.labelRotationRight.Size = new System.Drawing.Size(150, 25);
             this.labelRotationRight.TabIndex = 8;
             this.labelRotationRight.Text = "Obrót w prawo";
             // 
             // buttonRL_90
             // 
-            this.buttonRL_90.Location = new System.Drawing.Point(111, 334);
+            this.buttonRL_90.Location = new System.Drawing.Point(221, 642);
+            this.buttonRL_90.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRL_90.Name = "buttonRL_90";
-            this.buttonRL_90.Size = new System.Drawing.Size(39, 29);
+            this.buttonRL_90.Size = new System.Drawing.Size(77, 56);
             this.buttonRL_90.TabIndex = 9;
             this.buttonRL_90.Text = "90°";
             this.buttonRL_90.UseVisualStyleBackColor = true;
@@ -105,9 +110,10 @@
             // 
             // buttonRL_270
             // 
-            this.buttonRL_270.Location = new System.Drawing.Point(21, 334);
+            this.buttonRL_270.Location = new System.Drawing.Point(43, 642);
+            this.buttonRL_270.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRL_270.Name = "buttonRL_270";
-            this.buttonRL_270.Size = new System.Drawing.Size(39, 29);
+            this.buttonRL_270.Size = new System.Drawing.Size(77, 56);
             this.buttonRL_270.TabIndex = 10;
             this.buttonRL_270.Text = "270°";
             this.buttonRL_270.UseVisualStyleBackColor = true;
@@ -115,9 +121,10 @@
             // 
             // buttonRR_90
             // 
-            this.buttonRR_90.Location = new System.Drawing.Point(111, 278);
+            this.buttonRR_90.Location = new System.Drawing.Point(221, 535);
+            this.buttonRR_90.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRR_90.Name = "buttonRR_90";
-            this.buttonRR_90.Size = new System.Drawing.Size(39, 29);
+            this.buttonRR_90.Size = new System.Drawing.Size(77, 56);
             this.buttonRR_90.TabIndex = 11;
             this.buttonRR_90.Text = "90°";
             this.buttonRR_90.UseVisualStyleBackColor = true;
@@ -125,9 +132,10 @@
             // 
             // buttonRR_180
             // 
-            this.buttonRR_180.Location = new System.Drawing.Point(64, 278);
+            this.buttonRR_180.Location = new System.Drawing.Point(128, 535);
+            this.buttonRR_180.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRR_180.Name = "buttonRR_180";
-            this.buttonRR_180.Size = new System.Drawing.Size(39, 29);
+            this.buttonRR_180.Size = new System.Drawing.Size(77, 56);
             this.buttonRR_180.TabIndex = 12;
             this.buttonRR_180.Text = "180°";
             this.buttonRR_180.UseVisualStyleBackColor = true;
@@ -135,9 +143,10 @@
             // 
             // buttonRR_270
             // 
-            this.buttonRR_270.Location = new System.Drawing.Point(21, 278);
+            this.buttonRR_270.Location = new System.Drawing.Point(43, 535);
+            this.buttonRR_270.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonRR_270.Name = "buttonRR_270";
-            this.buttonRR_270.Size = new System.Drawing.Size(39, 29);
+            this.buttonRR_270.Size = new System.Drawing.Size(77, 56);
             this.buttonRR_270.TabIndex = 13;
             this.buttonRR_270.Text = "270°";
             this.buttonRR_270.UseVisualStyleBackColor = true;
@@ -145,9 +154,10 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(96, 394);
+            this.buttonSave.Location = new System.Drawing.Point(192, 758);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(149, 44);
             this.buttonSave.TabIndex = 14;
             this.buttonSave.Text = "Zapisz";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -156,43 +166,48 @@
             // labelResolution
             // 
             this.labelResolution.AutoSize = true;
-            this.labelResolution.Location = new System.Drawing.Point(51, 34);
+            this.labelResolution.Location = new System.Drawing.Point(101, 65);
+            this.labelResolution.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelResolution.Name = "labelResolution";
-            this.labelResolution.Size = new System.Drawing.Size(75, 13);
+            this.labelResolution.Size = new System.Drawing.Size(151, 25);
             this.labelResolution.TabIndex = 15;
             this.labelResolution.Text = "Rozdzielczość";
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.Size = new System.Drawing.Size(5, 1041);
             this.splitter1.TabIndex = 16;
             this.splitter1.TabStop = false;
             // 
             // labelResolutionHeight
             // 
             this.labelResolutionHeight.AutoSize = true;
-            this.labelResolutionHeight.Location = new System.Drawing.Point(59, 56);
+            this.labelResolutionHeight.Location = new System.Drawing.Point(117, 108);
+            this.labelResolutionHeight.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelResolutionHeight.Name = "labelResolutionHeight";
-            this.labelResolutionHeight.Size = new System.Drawing.Size(60, 13);
+            this.labelResolutionHeight.Size = new System.Drawing.Size(117, 25);
             this.labelResolutionHeight.TabIndex = 18;
             this.labelResolutionHeight.Text = "Wysokość:";
             // 
             // labelResolutionWidth
             // 
             this.labelResolutionWidth.AutoSize = true;
-            this.labelResolutionWidth.Location = new System.Drawing.Point(59, 94);
+            this.labelResolutionWidth.Location = new System.Drawing.Point(117, 181);
+            this.labelResolutionWidth.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelResolutionWidth.Name = "labelResolutionWidth";
-            this.labelResolutionWidth.Size = new System.Drawing.Size(57, 13);
+            this.labelResolutionWidth.Size = new System.Drawing.Size(113, 25);
             this.labelResolutionWidth.TabIndex = 19;
             this.labelResolutionWidth.Text = "Szerokość";
             // 
             // numericResolutionHeight
             // 
-            this.numericResolutionHeight.Location = new System.Drawing.Point(30, 71);
+            this.numericResolutionHeight.Location = new System.Drawing.Point(60, 136);
+            this.numericResolutionHeight.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.numericResolutionHeight.Maximum = new decimal(new int[] {
-            1080,
+            1000,
             0,
             0,
             0});
@@ -202,19 +217,21 @@
             0,
             0});
             this.numericResolutionHeight.Name = "numericResolutionHeight";
-            this.numericResolutionHeight.Size = new System.Drawing.Size(120, 20);
+            this.numericResolutionHeight.Size = new System.Drawing.Size(240, 31);
             this.numericResolutionHeight.TabIndex = 20;
             this.numericResolutionHeight.Value = new decimal(new int[] {
-            1080,
+            1000,
             0,
             0,
             0});
+            this.numericResolutionHeight.ValueChanged += new System.EventHandler(this.numericResolutionHeight_ValueChanged);
             // 
             // numericResolutionWidth
             // 
-            this.numericResolutionWidth.Location = new System.Drawing.Point(30, 110);
+            this.numericResolutionWidth.Location = new System.Drawing.Point(60, 211);
+            this.numericResolutionWidth.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.numericResolutionWidth.Maximum = new decimal(new int[] {
-            1920,
+            800,
             0,
             0,
             0});
@@ -224,10 +241,10 @@
             0,
             0});
             this.numericResolutionWidth.Name = "numericResolutionWidth";
-            this.numericResolutionWidth.Size = new System.Drawing.Size(120, 20);
+            this.numericResolutionWidth.Size = new System.Drawing.Size(240, 31);
             this.numericResolutionWidth.TabIndex = 21;
             this.numericResolutionWidth.Value = new decimal(new int[] {
-            1920,
+            700,
             0,
             0,
             0});
@@ -235,9 +252,10 @@
             // labelScaningType
             // 
             this.labelScaningType.AutoSize = true;
-            this.labelScaningType.Location = new System.Drawing.Point(41, 142);
+            this.labelScaningType.Location = new System.Drawing.Point(83, 272);
+            this.labelScaningType.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelScaningType.Name = "labelScaningType";
-            this.labelScaningType.Size = new System.Drawing.Size(85, 13);
+            this.labelScaningType.Size = new System.Drawing.Size(168, 25);
             this.labelScaningType.TabIndex = 22;
             this.labelScaningType.Text = "Typ skanowania";
             // 
@@ -245,18 +263,20 @@
             // 
             this.domainScaningSelector.Items.Add("Skanowanie Czarno-Białe");
             this.domainScaningSelector.Items.Add("Kolorowe");
-            this.domainScaningSelector.Location = new System.Drawing.Point(9, 158);
+            this.domainScaningSelector.Location = new System.Drawing.Point(19, 304);
+            this.domainScaningSelector.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.domainScaningSelector.Name = "domainScaningSelector";
-            this.domainScaningSelector.Size = new System.Drawing.Size(162, 20);
+            this.domainScaningSelector.Size = new System.Drawing.Size(324, 31);
             this.domainScaningSelector.TabIndex = 23;
             this.domainScaningSelector.Text = "Tryb skanowania";
             // 
             // labelSaveFileType
             // 
             this.labelSaveFileType.AutoSize = true;
-            this.labelSaveFileType.Location = new System.Drawing.Point(27, 192);
+            this.labelSaveFileType.Location = new System.Drawing.Point(53, 369);
+            this.labelSaveFileType.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelSaveFileType.Name = "labelSaveFileType";
-            this.labelSaveFileType.Size = new System.Drawing.Size(116, 13);
+            this.labelSaveFileType.Size = new System.Drawing.Size(231, 25);
             this.labelSaveFileType.TabIndex = 24;
             this.labelSaveFileType.Text = "Typ pliku Wyjściowego";
             // 
@@ -267,25 +287,30 @@
             this.domainFormatSelector.Items.Add("TIFF");
             this.domainFormatSelector.Items.Add("BMP");
             this.domainFormatSelector.Items.Add("REL");
-            this.domainFormatSelector.Location = new System.Drawing.Point(30, 217);
+            this.domainFormatSelector.Location = new System.Drawing.Point(60, 418);
+            this.domainFormatSelector.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.domainFormatSelector.Name = "domainFormatSelector";
-            this.domainFormatSelector.Size = new System.Drawing.Size(120, 20);
+            this.domainFormatSelector.Size = new System.Drawing.Size(240, 31);
             this.domainFormatSelector.TabIndex = 25;
             this.domainFormatSelector.Text = "Format";
+            this.domainFormatSelector.SelectedItemChanged += new System.EventHandler(this.domainFormatSelector_SelectedItemChanged);
             // 
             // pictureBoxScan
             // 
-            this.pictureBoxScan.Location = new System.Drawing.Point(195, 12);
+            this.pictureBoxScan.Location = new System.Drawing.Point(389, 15);
+            this.pictureBoxScan.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.pictureBoxScan.Name = "pictureBoxScan";
-            this.pictureBoxScan.Size = new System.Drawing.Size(593, 426);
+            this.pictureBoxScan.Size = new System.Drawing.Size(820, 1002);
+            this.pictureBoxScan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxScan.TabIndex = 26;
             this.pictureBoxScan.TabStop = false;
+            this.pictureBoxScan.Click += new System.EventHandler(this.pictureBoxScan_Click);
             // 
             // SkaningMode
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1232, 1041);
             this.Controls.Add(this.pictureBoxScan);
             this.Controls.Add(this.domainFormatSelector);
             this.Controls.Add(this.labelSaveFileType);
@@ -307,8 +332,10 @@
             this.Controls.Add(this.labelRotationLeft);
             this.Controls.Add(this.buttonRL_180);
             this.Controls.Add(this.buttonScan);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "SkaningMode";
             this.Text = "Skaner";
+            this.Load += new System.EventHandler(this.SkaningMode_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericResolutionHeight)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericResolutionWidth)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScan)).EndInit();
--- Urządzenia Peryferyjne Lab - 1 - Skaner/Urządzenia Peryferyjne Lab - 1 - Skaner/Form1.cs
+++ Urządzenia Peryferyjne Lab - 1 - Skaner/Urządzenia Peryferyjne Lab - 1 - Skaner/Form1.cs
@@ -9,21 +9,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WIA;
+
 
 
 namespace Urządzenia_Peryferyjne_Lab___1___Skaner
 {
     public partial class SkaningMode : Form
     {
+
         Image obraz;
         public SkaningMode()
         {
             InitializeComponent();
         }
 
+        private void scan() {
+
+        }
+
         private void buttonScan_Click(object sender, EventArgs e)
         {
-            obraz = new Bitmap("s1.bmp");
+            const string wiaFormatJPEG = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}";
+
+            int color = domainScaningSelector.SelectedIndex;
+            if (color == -1) {
+                MessageBox.Show("Nie wybrano trybu skanowania.");
+                return;
+            }
+                  
+            var deviceManager = new DeviceManager();
+            DeviceInfo firstScanner = null;
+            foreach (DeviceInfo deviceInfo in deviceManager.DeviceInfos)
+            {
+                if (deviceInfo.Type == WiaDeviceType.ScannerDeviceType)
+                {
+                    firstScanner = deviceInfo;
+                    break;
+                }
+            }
+
+            if (firstScanner != null)
+            {
+                Console.WriteLine("Znaleziono skaner: " + firstScanner.Properties["Name"].get_Value());
+
+                var device = firstScanner.Connect();
+                var scannerItem = device.Items[1] as WIA.Item;
+               
+                scannerItem.Properties["6147"].set_Value(600);//DPI- 600             
+
+                switch (color) {
+                    case 0:
+                        scannerItem.Properties["6146"].set_Value(2);//Czarno-Białe
+                        break;
+                    case 1:
+                        scannerItem.Properties["6146"].set_Value(1);//Kolorowe
+                        break;
+                    
+                }
+
+                int hight = (int)numericResolutionHeight.Value;
+                int width = (int)numericResolutionWidth.Value;
+
+                scannerItem.Properties["Horizontal Start Position"].set_Value(1);
+                scannerItem.Properties["Horizontal Start Position"].set_Value(1);
+                scannerItem.Properties["Horizontal Resolution"].set_Value(100);
+                scannerItem.Properties["Vertical Resolution"].set_Value(90);
+                scannerItem.Properties["Horizontal Extent"].set_Value(700);
+                scannerItem.Properties["Vertical Extent"].set_Value(hight);
+
+
+                foreach (Property property in scannerItem.Properties) {
+                    Console.WriteLine($"Nazwa właściwości: {property.Name}");
+                    Console.WriteLine($"Typ Danych: {property.Type}");
+                    Console.WriteLine($"Wartość:{property.get_Value()}");
+                    Console.WriteLine();
+                }
+
+                var imgFile = (ImageFile)scannerItem.Transfer(wiaFormatJPEG);
+                using (MemoryStream stream = new MemoryStream((byte[])imgFile.FileData.get_BinaryData())) {
+                    obraz = Image.FromStream(stream);
+                }
+                    
+                    Console.WriteLine("Obraz został pomyślnie zeskanowany i zapisany jako skan.jpg");
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono urządzenia skanującego.");
+            }
+
             pictureBoxScan.Image = obraz;
         }
 
@@ -69,7 +143,6 @@ namespace Urządzenia_Peryferyjne_Lab___1___Skaner
         private void buttonSave_Click(object sender, EventArgs e)
         {
             int code = domainFormatSelector.SelectedIndex;
-            Console.WriteLine(code);
             switch (code)
             {
                 case -1:
@@ -87,31 +160,69 @@ namespace Urządzenia_Peryferyjne_Lab___1___Skaner
                             File.WriteAllBytes("obraz.jpg", bytes);
                         }
                         MessageBox.Show("Obraz zapisany");
-
                     }
                     break;
                 case 1://PNG
+                    if (folderBrowserSavingFile.ShowDialog() == DialogResult.OK)
+                    {
+                        string sciezkaDoFolderu = folderBrowserSavingFile.SelectedPath;
+                        Console.WriteLine(sciezkaDoFolderu);
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            obraz.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                            byte[] bytes = memoryStream.ToArray();
+                            File.WriteAllBytes("obraz.png", bytes);
+                        }
+                        MessageBox.Show("Obraz zapisany");
+                    }
                     break;
                 case 2://TIFF
+                    if (folderBrowserSavingFile.ShowDialog() == DialogResult.OK)
+                    {
+                        string sciezkaDoFolderu = folderBrowserSavingFile.SelectedPath;
+                        Console.WriteLine(sciezkaDoFolderu);
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            obraz.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Tiff);
+                            byte[] bytes = memoryStream.ToArray();
+                            File.WriteAllBytes("obraz.tiff", bytes);
+                        }
+                        MessageBox.Show("Obraz zapisany");
+                    }
                     break;
                 case 3://BMP
+                    if (folderBrowserSavingFile.ShowDialog() == DialogResult.OK)
+                    {
+                        string sciezkaDoFolderu = folderBrowserSavingFile.SelectedPath;
+                        Console.WriteLine(sciezkaDoFolderu);
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            obraz.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                            byte[] bytes = memoryStream.ToArray();
+                            File.WriteAllBytes("obraz.bmp", bytes);
+                        }
+                        MessageBox.Show("Obraz zapisany");
+                    }
                     break;
                 case 4://REL
                     break;
             }
         }
-        // Metoda do uzyskania kodera dla określonego formatu obrazu
-        private static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+        private void SkaningMode_Load(object sender, EventArgs e)
         {
-            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
-            foreach (ImageCodecInfo codec in codecs)
-            {
-                if (codec.FormatID == format.Guid)
-                {
-                    return codec;
-                }
-            }
-            return null;
+
+        }
+
+        private void pictureBoxScan_Click(object sender, EventArgs e) {
+
+        }
+
+        private void numericResolutionHeight_ValueChanged(object sender, EventArgs e) {
+
+        }
+
+        private void domainFormatSelector_SelectedItemChanged(object sender, EventArgs e) {
+
         }
     }
 }
